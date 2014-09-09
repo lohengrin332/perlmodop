e4c54f80e999d7c4622b6d3ab144cce474d497d3
J. David Lewis <david.lewis@tigerlms.com>
2014-09-09 01:51:35 +0000

    Convert to Moo, add POD.

--- lib/ModOp.pm
+++ lib/ModOp.pm
@@ -1,12 +1,34 @@
 package ModOp;
 
-use Moose;
+use Moo;
 use Try::Tiny;
 
+=head1 NAME
+
+ModOp.pm
+
+=head1 DESCRIPTION
+
+Used to parse package names into actual package files, then either:
+1> Open those files in new VIM tabs.
+2> Process those files with "TlistAddFiles" and open a tags window (TlistOpen).
+
+=cut
+
+=head2 ModOp::ProprietaryRole
+
+    Allow for customization to include proprietary or private code.
+
+    If, for instance, your organization uses a custom library to manipulate the
+    @INC paths, ModOp::ProprietaryRole can be created and added to the default
+    Perl path, or PROPRIETARY_VIM_PATH can be defined, getINC can be overridden
+    or extended, and that custom @INC path library can be called, allowing
+    ModOp to find Perl packages in your custom locations.
+
+=cut
 try {
-    # bomb out so we don't try to add a non-existant path to @INC, and won't try to use a non-existant file
-    die unless($ENV{PROPRIETARY_VIM_PATH} && -r "$ENV{PROPRIETARY_VIM_PATH}/lib/ModOp/ProprietaryRole.pm");
-    unshift(@INC, "$ENV{PROPRIETARY_VIM_PATH}/lib");
+    unshift(@INC, "$ENV{PROPRIETARY_VIM_PATH}/lib")
+        if($ENV{PROPRIETARY_VIM_PATH} && -r "$ENV{PROPRIETARY_VIM_PATH}/lib/ModOp/ProprietaryRole.pm");
     with qw(
         ModOp::ProprietaryRole
     );
@@ -17,76 +39,33 @@ try {
 has 'curwin' => ( is => 'rw', required => 1, );
 has 'debug'  => ( is => 'rw', default => 0, );
 
-sub getFilesUnderCursor {
+=head2
+
+    Utilizing the VIM API, find any package names on the current line of Perl
+    code and open them in new tabs to the right of the current tab.
+
+=cut
+sub openSourceFile {
     my $self = shift;
-
-    my ($row, $col) = $self->curwin->Cursor();
-    my $line = $self->curwin->Buffer->Get($row);
-
-    return $self->getModulesFromLine($line);
-}
-
-sub getModulesFromLine {
-    my $self = shift;
-    my $line = shift;
-
-    my @parts = $self->splitLineToParts($line);
-
-    my @modules;
-    for my $part(@parts) {
-        push(@modules, $part) unless($part =~ m/\A(use|require|extends|parent|isa|with|qw)\z/);
+    my @files = $self->getFilesUnderCursor();
+    unless(scalar(@files)) {
+        VIM::Msg('No files found') if($self->debug);
+        return;
     }
-
-    my @files;
-    for my $module(@modules) {
-        my $file_name = $self->getFileNameFromModule($module);
-        my $full_file_name = $self->getFilePath($file_name);
-        push(@files, $full_file_name) if($full_file_name && -r $full_file_name);
+    for my $file(reverse(@files)) {
+        VIM::DoCommand("tabedit $file\ntabprevious");
     }
-
-    return @files;
+    VIM::DoCommand('tabnext');
 }
 
-sub splitLineToParts {
-    my $self = shift;
-    my $line = shift;
+=head2
 
-    my @parts;
-    # split the line on characters which are ineligible for module names
-    for my $part(split(qr{[^A-Za-z0-9_:]+}, $line)) {
-        VIM::Msg($part) if($self->debug);
-        # confirm that the part is a valid module name
-        push(@parts, $part) if($part =~ m{\A [A-Z_a-z] [0-9A-Z_a-z]* (?: :: [0-9A-Z_a-z]+)* \z}x);
-    }
+    Utilizing the VIM API, find any package names on the current line of Perl
+    code and load them into the tags list, then open the tags list.
+    This requires the "taglist" plugin, found here:
+    http://www.vim.org/scripts/script.php?script_id=273
 
-    return @parts;
-}
-
-sub getFileNameFromModule {
-    my $self = shift;
-    my $module = shift;
-
-    return join('/', split('::', $module)) . ".pm";
-}
-
-sub getFilePath {
-    my $self = shift;
-    my $file_name = shift;
-
-    for my $dir($self->getINC) {
-        return "$dir/$file_name" if(-f "$dir/$file_name");
-    }
-
-    VIM::Msg("File $file_name not found in \@INC") if($self->debug);
-    return undef;
-}
-
-sub getINC {
-    my $self = shift;
-
-    return @INC;
-}
-
+=cut
 sub loadSourceFile {
     my $self = shift;
     my @files = $self->getFilesUnderCursor();
@@ -99,17 +78,111 @@ sub loadSourceFile {
     VIM::DoCommand('TlistOpen');
 }
 
-sub openSourceFile {
+=head2 getFilesUnderCursor
+
+    Uses the defined curwin to find the line where the cursor currently rests,
+    then passes that line to another subroutine for processing.
+
+=cut
+sub getFilesUnderCursor {
     my $self = shift;
-    my @files = $self->getFilesUnderCursor();
-    unless(scalar(@files)) {
-        VIM::Msg('No files found') if($self->debug);
-        return;
+
+    my ($row, $col) = $self->curwin->Cursor();
+    my $line = $self->curwin->Buffer->Get($row);
+
+    return $self->getPackagesFromLine($line);
+}
+
+=head2 getPackagesFromLine
+
+    Given a line of perl code, find valid package name strings, exclude known
+    inclusion verbs (e.g. use/require/parent), pass each package name to other
+    subroutines to parse them into actual file paths, then return this list of
+    file paths.
+
+=cut
+sub getPackagesFromLine {
+    my $self = shift;
+    my $line = shift;
+
+    my @parts = $self->splitLineToParts($line);
+
+    my @packages;
+    for my $part(@parts) {
+        push(@packages, $part) unless($part =~ m/\A(use|require|extends|parent|isa|with|qw)\z/);
     }
-    for my $file(reverse(@files)) {
-        VIM::DoCommand("tabedit $file\ntabprevious");
+
+    my @files;
+    for my $package(@packages) {
+        my $file_name = $self->getFileNameFromPackage($package);
+        my $full_file_name = $self->getFilePath($file_name);
+        push(@files, $full_file_name) if($full_file_name && -r $full_file_name);
     }
-    VIM::DoCommand('tabnext');
+
+    return @files;
+}
+
+=head2
+
+    Split a line into a list of potential package names, excluding characters
+    which are not valid as parts of a package name.
+
+=cut
+sub splitLineToParts {
+    my $self = shift;
+    my $line = shift;
+
+    my @parts;
+    # split the line on characters which are ineligible for package names
+    for my $part(split(qr{[^A-Za-z0-9_:]+}, $line)) {
+        VIM::Msg($part) if($self->debug);
+        # confirm that the part is a valid package name
+        # regex found here: http://blogs.perl.org/users/michael_g_schwern/2011/10/how-not-to-load-a-module-or-bad-interfaces-make-good-people-do-bad-things.html
+        push(@parts, $part) if($part =~ m{\A [A-Z_a-z] [0-9A-Z_a-z]* (?: :: [0-9A-Z_a-z]+)* \z}x);
+    }
+
+    return @parts;
+}
+
+=head2
+
+    Convert a Perl package name to a partial file path.
+
+=cut
+sub getFileNameFromPackage {
+    my $self = shift;
+    my $package = shift;
+
+    return join('/', split('::', $package)) . ".pm";
+}
+
+=head2
+
+    Given a partial file path, searh @INC paths for a matching file.
+
+=cut
+sub getFilePath {
+    my $self = shift;
+    my $file_name = shift;
+
+    for my $dir($self->getINC) {
+        return "$dir/$file_name" if(-f "$dir/$file_name");
+    }
+
+    VIM::Msg("File $file_name not found in \@INC") if($self->debug);
+    return undef;
+}
+
+=head2
+
+    Return @INC.  This can be overridden or (more appropriately) extended via
+    an "around" modifier to pull in extra paths if desired.
+
+=cut
+sub getINC {
+    my $self = shift;
+
+    return @INC;
 }
 
 
